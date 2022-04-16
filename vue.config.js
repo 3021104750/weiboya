e1a6761c11f2e9e68d2b89822f019ce38ee4831f
Korin <386303472@qq.com>
2022-04-16 20:10:04 +0800

    again commit

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,16 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/weiboya/" : "/"
+  publicPath: process.env.NODE_ENV === "production" ? "/weiboya/" : "./",
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true, //是否跨域
+        pathRewrite: {
+          '^/api': ''
+        }
+      }
+    },
+    host: 'localhost', //
+    port: 8080, //
+  }
 };
