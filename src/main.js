efcc2c8edf1146718390df8c4df54cc9dcd9feaf
Korin <386303472@qq.com>
2022-04-16 20:19:49 +0800

    json in assets

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from './router';
 import store from './store';
 import 'normalize.css';
 import './assets/css/iconfont.css';
+import './assets/css/initCss.css';
 
 Vue.config.productionTip = false;
 
--- vue.config.js
+++ vue.config.js
@@ -1,16 +1,3 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/weiboya/" : "./",
-  devServer: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8080',
-        changeOrigin: true, //是否跨域
-        pathRewrite: {
-          '^/api': ''
-        }
-      }
-    },
-    host: 'localhost', //
-    port: 8080, //
-  }
+  publicPath: process.env.NODE_ENV === "production" ? "/weiboya/" : "./"
 };
